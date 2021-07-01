465b8dcfef07c687b788e238d017c41d2585e2d6
Epsilon Knowledge <67329374+Epsilon-Knowledge@users.noreply.github.com>
2021-07-01 09:07:30 +0900

    引数に受けた文字列を表示させるよう修正。

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import yaml
+import argparse
 
 CONFIG_FILE =  './config.yml'
 
-
 def main():
+
+  # configファイルの読み込み
   with open(CONFIG_FILE, 'r') as yml:
     config = yaml.safe_load(yml)
 
   print('hello world')
   print(config)
+
+  # 引数の指定
+  parser = argparse.ArgumentParser()
+  parser.add_argument("mail_body", help = "the body of mail you want to transfer to slack.")
+  args = parser.parse_args()
+
+  print(args.mail_body)
 
 if __name__ == '__main__':
   main()
