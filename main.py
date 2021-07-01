aec649cd47e4e1b686d252ccae4fbaad45d6ed31
Epsilon Knowledge <67329374+Epsilon-Knowledge@users.noreply.github.com>
2021-07-01 09:16:01 +0900

    インデントを修正。

--- main.py
+++ main.py
@@ -5,19 +5,19 @@ CONFIG_FILE =  './config.yml'
 
 def main():
 
-  # configファイルの読み込み
-  with open(CONFIG_FILE, 'r') as yml:
-    config = yaml.safe_load(yml)
+    # configファイルの読み込み
+    with open(CONFIG_FILE, 'r') as yml:
+      config = yaml.safe_load(yml)
 
-  print('hello world')
-  print(config)
+    print('hello world')
+    print(config)
 
   # 引数の指定
-  parser = argparse.ArgumentParser()
-  parser.add_argument("mail_body", help = "the body of mail you want to transfer to slack.")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mail_body", help = "the body of mail you want to transfer to slack.")
+    args = parser.parse_args()
 
-  print(args.mail_body)
+    print(args.mail_body)
 
 if __name__ == '__main__':
-  main()
+    main()
