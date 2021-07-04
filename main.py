bac8f6b8340566691822f449c0d5049da7743c31
Epsilon Knowledge <67329374+Epsilon-Knowledge@users.noreply.github.com>
2021-07-04 19:30:49 +0900

    メールをパースして必要な情報を取得。

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yaml
 import argparse
 import sys
 import models
+import email
 
 CONFIG_FILE =  '/usr/local/slack_sender/config.yml'
 
@@ -9,27 +10,33 @@ def main():
 
     # 引数の指定
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', help = 'debug mode', action='store_true')
+    parser.add_argument('-d', help = 'debug mode', action = 'store_true')
     args = parser.parse_args()
 
     # configファイルの読み込み
     with open(CONFIG_FILE, 'r') as yml:
       config = yaml.safe_load(yml)
 
+
     # 標準入力を受け取り
     stdin = sys.stdin.read()
 
     # DEBUG
-    print('stdin: ' + stdin)
+    # print('stdin: ' + stdin)
 
-    # DEBUG
-    print('arguments: ' + str(args.d))
+    # # DEBUG
+    # print('arguments: ' + str(args.d))
+    msg = email.message_from_string(stdin)
+    print(type(msg))
+    print(msg.get('Date'))
+    print(msg.get('Return-Path'))
+    print(msg.get('From'))
+    print(msg.get('Subject'))
+    #print(msg.items())
+    print(msg.get_payload(decode = False))
 
-    # jsonに整形
 
-
-
-    # 本文を作成？
+    # 本文を作成
 
     # slackに送信
 
